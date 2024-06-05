d0e321bb6fa25f27aeeae48c425eda43790a49b2
LudimarPadilha <ludi_._mar@hotmail.com>
2024-06-05 19:34:21 -0300

    Alteração de estrutura

--- app/db_access/updateTable.py
+++ app/db_access/updateTable.py
@@ -10,11 +10,13 @@ def executasql():
   registrosinseridos = [] 
   arquivo = "./sql"
   sql_arquivos = glob.glob(f"{arquivo}/*.sql")
+
   for file_path in sql_arquivos:
       with open(file_path, "r") as sql_arquivos:
           commands = sql_arquivos.read()
           cur = conexao.cursor()
           cur.execute(commands)
-          conexao.commit()
-          registrosinseridos.append((cur.fetchall()))
-      return len(registrosinseridos)
+          registrosinseridos.extend(cur.fetchall())
+      conexao.commit()
+      
+  return len(registrosinseridos)  
